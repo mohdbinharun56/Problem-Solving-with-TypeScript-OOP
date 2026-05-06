97d71641bc178e6f52a4f4a9cad41ff57bc86e0a
mohdbinharun56 <arahaman461@gmail.com>
2026-05-06 22:14:47 +0600

    solved second problem

--- solutions.ts
+++ solutions.ts
@@ -1,5 +1,16 @@
 // Problem 1
 
 function filterEvenNumbers(numbers: number[]): number[]{
-    return numbers.filter(number=> number%2 ===0 && number);
+    return numbers.filter((number: number)=> number%2 ===0 && number);
 }
+
+// Problem 2
+
+const reverseString = (reverseString: string): string => {
+    let reversed: string = "";
+    for(let i = reverseString.length -1; i>=0; i--){
+        reversed+= reverseString[i]
+    }
+    return reversed;
+}
+
